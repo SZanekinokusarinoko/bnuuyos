81230340b2ffefc600590975296ab5c8424cd969
SZ姉貴の鎖鋸 <131784806+SZanekinokusarinoko@users.noreply.github.com>
2023-09-29 20:19:55 -0500

    Add files via upload

---
+++ bnuuyos.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+git clone https://github.com/rozniak/xfce-winxp-tc.github
+cd xfce-winxp-tc/packaging/
+chmod +x buildall.sh chkdeps.sh
+apt update && apt upgrade
+apt install $(./chkdeps.sh -l | cut -d':' -f2 | tr '\n' ' ')
+echo "Now building Luna GUI, please wait warmly!"
+./buildall.sh
+
+ed /etc/os-release 1 c NAME="bnuuyOS"
+ed /etc/os-release 5 c PRETTY_NAME="bnuuyOS Alpha 2023.23.09"
+
