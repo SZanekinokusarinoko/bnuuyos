848e478aec728603a831457fa42e148ca0d47c92
SZ姉貴の鎖鋸 <seacount1337@gmail.com>
2023-10-03 12:47:53 -0500

    dpkg might finally work, also removed redundent line

--- bnuuyos.sh
+++ bnuuyos.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 git clone https://github.com/rozniak/xfce-winxp-tc.git
-cd xfce-winxp-tc/packaging/
-chmod +x buildall.sh chkdeps.sh
+chmod +x ~/xfce-winxp-tc/packaging/buildall.sh chkdeps.sh
 apt update && apt upgrade
 apt install $(./chkdeps.sh -l | cut -d':' -f2 | tr '\n' ' ')
 # apparently cmake isn't one of the dependencies checked, maybe adjust later?
 apt install cmake
 ./buildall.sh
 
-# copy default binary folder to a custom one because cmake
-cd xfce-winxp-tc/packaging/xptc
-cp -R $(ls | grep -w 'master') ~/bnuuyos/bin
-dpkg -i ~/bnuuyos/bin/deb/x86_64
+# grep magic needed to install binaries, blame cmake
+sudo dpkg -i xfce-winxp-tc/packaging/xptc/$(ls | grep -w 'master')/deb/x86_64
 
 ed /etc/os-release 1 c NAME="bnuuyOS" w /etc/os-release
 ed /etc/os-release 5 c PRETTY_NAME="bnuuyOS Alpha 2023.02.10" w /etc/os-release
