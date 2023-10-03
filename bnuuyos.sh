ff7454140532d093f6acbf0d183a84d941e02852
SZ姉貴の鎖鋸 <seacount1337@gmail.com>
2023-10-02 22:16:41 -0500

    potentially fix issue with binaries not installing, same goes for os-release files not being saved

--- bnuuyos.sh
+++ bnuuyos.sh
@@ -5,9 +5,13 @@ cd xfce-winxp-tc/packaging/
 chmod +x buildall.sh chkdeps.sh
 apt update && apt upgrade
 apt install $(./chkdeps.sh -l | cut -d':' -f2 | tr '\n' ' ')
-echo "Now building Luna GUI, please wait warmly!"
+# apparently cmake isn't one of the dependencies checked, maybe adjust later?
+apt install cmake
 ./buildall.sh
 
-ed /etc/os-release 1 c NAME="bnuuyOS"
-ed /etc/os-release 5 c PRETTY_NAME="bnuuyOS Alpha 2023.23.09"
+# copy default binary folder to a custom one because cmake
+mv $(ls | grep -w 'master') built
+
+ed /etc/os-release 1 c NAME="bnuuyOS" w /etc/os-release
+ed /etc/os-release 5 c PRETTY_NAME="bnuuyOS Alpha 2023.23.09" w /etc/os-release
 
